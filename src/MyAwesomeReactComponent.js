acc30ddb76cb6b122dbd943d09c1146050640c0b
Fred Olivieri <frederic.olivieri@trinet.com>
2017-12-21 15:40:48 -0800

    adding files

---
+++ src/MyAwesomeReactComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const MyAwesomeReactComponent = () => (
+    <RaisedButton label="Default" />
+);
+
+export default MyAwesomeReactComponent;
