1ea8cd2f9f6c0262dc3681395c62038f3cbc7231
Fred Olivieri <frederic.olivieri@trinet.com>
2018-01-04 15:44:44 -0800

    StarWars-ified summary v2
    repeats

--- docs/index.js
+++ docs/index.js
@@ -32,26 +32,25 @@ const header = (
 
 const swSummary = (
   <div class="star-wars-intro">
+    <p id="start">A short time ago in a browser very, very close&hellip;</p>
 
-    <p class="intro-text">
-      A Brief History of Fred...
-    </p>
+    <h1>STAR WARS<sub>titles in CSS3</sub></h1>
 
-    <h2 class="main-logo">
-      <img src="img/star-wars-intro.png"/>
-    </h2>
 
-    <div class="main-content">
-        <div class="title-content">
-          <p class="content-header">SUMMARY OF EXPERIENCE<br/>A New Hope for ClearCapital</p>
-          <br/>
-            <p>He is a Senior software engineer with over 20 years of hands-on experience and expertise in application software design and development and involvement in all phases of Software Development Lifecycle (SDLC).</p>
-            <p>Responsible for having architected and developed multiple highly successful Java EE based commercial software applications fully internationalized, using the following JEE technologies: Session and Entity EJBs, CMP 2.0, Message Driven Beans, JMS, JDBC, JSP, JSF, Hibernate, Spring, JSTL, Servlets, Struts MVC framework.</p>
-            <p>He has extensive experience with agile software development techniques, such as Extreme Programming (XP) and Scrum methodologies (certified scrum master), as well as Rational Unified Process using UML ,Class-responsibility-collaboration (CRC) cards.</p>
-            <p>Further, he has expert level front end skills in HTML/DHTML/XHTML, JavaScript, Cascading Style Sheets (CSS), JSP, Servlets, XSL, JSON, Java Server Faces (JSF) including Oracle ADF Faces and Apache MyFaces, Grails, Ajax Component frameworks such as Node.js, Dojo, YUI, Angular JS, Facebook’s React, and Sencha Command/ExtJS.</p>
-            <p>Fred has exceptional leadership and management skills, experienced in leading local, remote, and offshore teams and is an experienced instructor for University of California, Irvine extension courses related to J2EE development: JSP, JDBC, UML, Intro to J2EE, Enterprise Java Beans. Also, he has conducted numerous corporate training courses on product usage.</p>
+    <div id="titles">
+      <div id="titlecontent">
 
-        </div>
+        <p class="center">EPISODE I<br />A NEW HOPE FOR CLEARCAPITAL</p>
+
+        <p>The company has been searching for someone to help their efforts.</p>
+
+        <p>Enter Fred Olivieri, a senior software engineer with over 20 years of hands-on experience and expertise in application software design and development and involvement in all phases of Software Development Lifecycle (SDLC).</p>
+        <p>He was responsible for having architected and developed multiple highly successful Java EE based commercial software applications fully internationalized, using the following JEE technologies: Session and Entity EJBs, CMP 2.0, Message Driven Beans, JMS, JDBC, JSP, JSF, Hibernate, Spring, JSTL, Servlets, Struts MVC framework.</p>
+        <p>He has extensive experience with agile software development techniques, such as Extreme Programming (XP) and Scrum methodologies (certified scrum master), as well as Rational Unified Process using UML ,Class-responsibility-collaboration (CRC) cards.</p>
+        <p>But perhaps more pertinent, he has expert level front end skills in HTML/DHTML/XHTML, JavaScript, Cascading Style Sheets (CSS), JSP, Servlets, XSL, JSON, Java Server Faces (JSF) including Oracle ADF Faces and Apache MyFaces, Grails, Ajax Component frameworks such as Node.js, Dojo, YUI, Angular JS, Facebook’s React, and Sencha Command/ExtJS.</p>
+        <p>Hoping to take advantage of his exceptional leadership and management skills, experienced in leading local, remote, and offshore teams, they are evaluating whether he can support their efforts.</p>
+        <p>Since he was instructor for University of California, Irvine extension courses related to J2EE development: JSP, JDBC, UML, Intro to J2EE, Enterprise Java Beans, they are hoping his leadership will provide the spark the company needs.</p>
+      </div>
     </div>
   </div>
 )
